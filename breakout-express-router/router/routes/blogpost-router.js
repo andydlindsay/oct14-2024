c3be9babe448aa2cb3868b266634b455214ff65d
Andy Lindsay <andydlindsay@gmail.com>
2024-11-15 14:51:03 -0800

    breakout over

---
+++ breakout-express-router/router/routes/blogpost-router.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const client = require('../db/connection');
+
+// middleware
+router.use((req, res, next) => {
+  // if (!req.cookies.userId) {
+  //   res.redirect('/login');
+  // }
+  console.log('inside the blogpost router');
+
+  next();
+});
+
+// GET /blogposts
+router.get('/', (req, res) => {
+  client.query('SELECT * FROM blogposts;')
+    .then((response) => {
+      const blogposts = response.rows;
+
+      res.json(blogposts);
+    })
+})
+
+// GET /blogposts/:id
+router.get('/:id', (req, res) => {
+  const blogpostId = req.params.id;
+  client.query('SELECT * FROM blogposts WHERE id = $1;', [blogpostId])
+    .then((response) => {
+      const blogpost = response.rows[0];
+      res.json(blogpost);
+    });
+})
+
+module.exports = router;
